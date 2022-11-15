c0300d046ab4d43821d5d8798d2ebc732ffe43ee
SeanECurrie <101378600+SeanECurrie@users.noreply.github.com>
2022-11-15 16:10:13 -0600

    first

---
+++ DAY 1/test.py
@@ -0,0 +1,21 @@
+# # Write a function to take in a sentence and assign a numeric value to each letter.
+# # No two letters should have the same numeric value, and each letter should have a
+# # dedicated value (so that the same letter does not have two numeric values).
+
+# def ranking(sentence):
+#     number = {}
+#     indi = []
+#     z = 0
+#     for i in sentence:
+#         if i.
+
+import re
+
+with open('regex_test.txt') as f:
+    data = f.read()
+    print(data)
+
+pattern = re.compile("^([A-Z]([a-z]+|.)\s*){2,3}$")
+match = pattern.findall(data)
+
+print(match)
